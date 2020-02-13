4e25efd4d0557286ec2042ef31494a64845b73e5
Joey O'Neill <40486240+joeyoneill@users.noreply.github.com>
2020-02-13 13:49:02 -0500

    Update models.py

--- home/models.py
+++ home/models.py
@@ -2,10 +2,3 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-
-#class Tweet(models.model):
-#	username = models.CharField(max_length = 15)
-#	content = models.TextField()
-#	likes = models.CharField()
-#	retweets = models.CharField()
-#	date_posted = models.DateTimeField(default=timezone.now)
